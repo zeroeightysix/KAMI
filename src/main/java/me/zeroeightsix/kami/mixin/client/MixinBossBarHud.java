6d7ae7575bff8aa12f0e84a302f90a6224dc821c
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-12-16 22:07:09 +0100

    Fix MixinBossBarHud

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinBossBarHud.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinBossBarHud.java
@@ -1,27 +1,27 @@
 package me.zeroeightsix.kami.mixin.client;
 
+import me.zeroeightsix.kami.KamiMod;
+import me.zeroeightsix.kami.event.events.RenderBossBarEvent;
 import net.minecraft.client.gui.hud.BossBarHud;
+import net.minecraft.client.gui.hud.ClientBossBar;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.Iterator;
+
 @Mixin(BossBarHud.class)
 public class MixinBossBarHud {
 
-    /*@Inject(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;"), cancellable = true)
-    public void onRender(CallbackInfo info, ClientBossBar clientBossBar) {
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;", shift = At.Shift.BY, by = 3), cancellable = true, locals = LocalCapture.PRINT)
+    public void onRender(CallbackInfo info, int i, int j, Iterator var3, ClientBossBar clientBossBar) {
         RenderBossBarEvent event = new RenderBossBarEvent(clientBossBar);
         KamiMod.EVENT_BUS.post(event);
         if (event.isCancelled()) {
             info.cancel();
         }
-    }*/
-
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;", shift = At.Shift.AFTER), cancellable = true, locals = LocalCapture.PRINT)
-    public void onRender(CallbackInfo info) {
     }
-
 
 }
