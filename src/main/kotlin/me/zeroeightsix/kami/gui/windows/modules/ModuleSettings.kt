f3e6b4b0f73f68ee9d6e50c6a68d1f6b88946661
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-01-09 22:04:18 +0100

    Style descriptions in module settings

--- src/main/kotlin/me/zeroeightsix/kami/gui/windows/modules/ModuleSettings.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/windows/modules/ModuleSettings.kt
@@ -1,14 +1,20 @@
 package me.zeroeightsix.kami.gui.windows.modules
 
+import glm_.vec4.Vec4
+import imgui.Col
+import imgui.ImGui.popStyleColor
+import imgui.ImGui.pushStyleColor
 import imgui.ImGui.sameLine
-import imgui.ImGui.text
+import imgui.ImGui.textWrapped
 import imgui.api.demoDebugInformations.Companion.helpMarker
 import me.zeroeightsix.kami.module.Module
 
 object ModuleSettings {
 
     operator fun invoke(module: Module, block: () -> Unit) {
-        text(module.description)
+        pushStyleColor(Col.Text, Vec4(.7f, .7f, .7f, 1f))
+        textWrapped(module.description)
+        popStyleColor()
         sameLine()
         helpMarker("Start dragging from this question mark to merge this module into another module window. Right click this question mark and press 'Detach' to seperate it into a new window.")
         block()
