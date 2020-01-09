2f97be528d17e514fa27b3d77d74aa3ec7bed0da
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-01-09 22:08:39 +0100

    More module window settings

--- src/main/kotlin/me/zeroeightsix/kami/gui/windows/KamiSettings.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/windows/KamiSettings.kt
@@ -3,19 +3,33 @@ package me.zeroeightsix.kami.gui.windows
 import imgui.ImGui.sameLine
 import imgui.api.demoDebugInformations
 import imgui.dsl.checkbox
+import imgui.dsl.collapsingHeader
 import imgui.dsl.window
 
 object KamiSettings {
     
     var settingsWindowOpen = false
     var swapModuleListButtons = false
+    var hideModuleDescriptions = false
+    var hideModuleMarker = false
 
     operator fun invoke() {
         if (settingsWindowOpen) {
             window("Settings", ::settingsWindowOpen) {
-                checkbox("Swap module list buttons", ::swapModuleListButtons) {}
-                sameLine()
-                demoDebugInformations.helpMarker("When enabled, right clicking modules will reveal their settings menu. Left clicking will toggle the module.")
+                collapsingHeader("Module windows") {
+                    checkbox("Swap list buttons", ::swapModuleListButtons) {}
+                    sameLine()
+                    demoDebugInformations.helpMarker("When enabled, right clicking modules will reveal their settings menu. Left clicking will toggle the module.")
+
+                    checkbox("Hide descriptions", ::hideModuleDescriptions) {}
+                    sameLine()
+                    demoDebugInformations.helpMarker("Hide module descriptions when its settings are opened.")
+
+                    checkbox("Hide help marker", ::hideModuleMarker) {}
+                    sameLine()
+                    demoDebugInformations.helpMarker("Hide the help marker (such as the one you are hovering right now) in module settings.")
+                }
+
             }
         }
     }
--- src/main/kotlin/me/zeroeightsix/kami/gui/windows/modules/ModuleSettings.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/windows/modules/ModuleSettings.kt
@@ -7,16 +7,23 @@ import imgui.ImGui.pushStyleColor
 import imgui.ImGui.sameLine
 import imgui.ImGui.textWrapped
 import imgui.api.demoDebugInformations.Companion.helpMarker
+import me.zeroeightsix.kami.gui.windows.KamiSettings
 import me.zeroeightsix.kami.module.Module
 
 object ModuleSettings {
 
     operator fun invoke(module: Module, block: () -> Unit) {
-        pushStyleColor(Col.Text, Vec4(.7f, .7f, .7f, 1f))
-        textWrapped(module.description)
-        popStyleColor()
-        sameLine()
-        helpMarker("Start dragging from this question mark to merge this module into another module window. Right click this question mark and press 'Detach' to seperate it into a new window.")
+        val markerShown = !KamiSettings.hideModuleMarker
+        if (!KamiSettings.hideModuleDescriptions) {
+            pushStyleColor(Col.Text, Vec4(.7f, .7f, .7f, 1f))
+            textWrapped(module.description)
+            popStyleColor()
+            if (markerShown)
+                sameLine()
+        }
+        if (markerShown) {
+            helpMarker("Start dragging from this question mark to merge this module into another module window. Right click this question mark and press 'Detach' to seperate it into a new window.")
+        }
         block()
 
         module.settingList.filter{ it.isVisible }.forEach {
