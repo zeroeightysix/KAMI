b103f053d6ecbd4ec3f3f4917057167127786a77
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-01-12 21:29:49 +0100

    Rewrite CompiledText, added colours

--- src/main/kotlin/me/zeroeightsix/kami/gui/widgets/TextPinnableWidget.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/widgets/TextPinnableWidget.kt
@@ -12,7 +12,6 @@ import me.zeroeightsix.kami.gui.KamiHud
 import me.zeroeightsix.kami.util.LagCompensator
 import me.zeroeightsix.kami.util.Wrapper
 import java.util.function.Supplier
-import java.util.regex.Pattern
 import kotlin.reflect.KMutableProperty0
 
 open class TextPinnableWidget(title: String, val text: ArrayList<CompiledText> = arrayListOf()) : PinnableWidget(title) {
@@ -75,59 +74,197 @@ open class TextPinnableWidget(title: String, val text: ArrayList<CompiledText> =
     }
     
     class CompiledText(text: String) {
-        companion object {
-            val pattern: Pattern = Pattern.compile("\\\$(\\w+)")
-        }
-        
-        private val parts: List<Supplier<String>>
+        val parts: List<Cmd>
 
         init {
-            parts = ArrayList()
-            val matcher = pattern.matcher(text)
-            var index = 0
-            
-            fun String.diff(start: Int, end: Int? = null): String {
-                val diff = when (end) {
-                    null -> substring(start)
-                    else -> substring(start, end)
-                }
-                if (diff.startsWith("\\")) {
-                    return diff.substring(1)
-                }
-                return diff
-            }
-            
-            while (matcher.find()) {
-                val lastIndex = index
-                index = matcher.start()
+            parts = text.toParts()
+        }
 
-                val diff = text.diff(lastIndex, index)
-                parts.add(Supplier { diff })
+        private fun String.toParts(colour: Int = 0xffffff): List<Cmd> {
+            val parts = ArrayList<Cmd>()
+            var colour = colour
+            var alpha = 0xff
 
-                val group = matcher.group(1)
-                parts.add(when (group) {
-                    "x" -> Supplier { Wrapper.getPlayer().pos.x.toString() }
-                    "y" -> Supplier { Wrapper.getPlayer().pos.y.toString() }
-                    "z" -> Supplier { Wrapper.getPlayer().pos.z.toString() }
-                    "yaw" -> Supplier { Wrapper.getPlayer().yaw.toString() }
-                    "pitch" -> Supplier { Wrapper.getPlayer().pitch.toString() }
-                    "tps" -> Supplier { LagCompensator.INSTANCE.tickRate.toString() }
-                    "username" -> Supplier { Wrapper.getMinecraft().session.username }
-                    else -> Supplier { "" }
-                })
-                
-                index = matcher.end()
+            var buf = ""
+            var escape = false
+
+            var obfuscated = false
+            var bold = false
+            var strike = false
+            var underline = false
+            var italic = false
+            var rainbow = false
+
+            fun pushPart(supplier: Supplier<String>) {
+                val cmd = Cmd(
+                    supplier,
+                    colour,
+                    obfuscated, bold, strike, underline, italic, rainbow,
+                    alpha
+                )
+                parts.add(cmd)
             }
-            val diff = text.diff(index)
-            parts.add(Supplier { diff })
+
+            fun pushPart() {
+                val str = "" + buf // copy str
+                if (str.isEmpty()) return
+                pushPart(Supplier { str })
+                buf = ""
+            }
+
+            fun pushReset() {
+                pushPart()
+
+                obfuscated = false
+                bold = false
+                strike = false
+                underline = false
+                italic = false
+                rainbow = false
+            }
+
+            val iterator = iterator()
+            while (iterator.hasNext()) {
+                val c = iterator.nextChar()
+                if (!iterator.hasNext()) { // Any special char is invalid now
+                    buf += c
+                } else if (escape && (c == '&' || c == '#' || c == '$')) {
+                    // We're escaping a special char.
+                    // We don't append the backslash.
+                    escape = false
+                    buf += c
+                } else when (c) {
+                    '&' -> { // Minecraft colour
+                        val next = iterator.nextChar()
+                        colour = when (next) {
+                            '0' -> { pushReset(); 0x000000 }
+                            '1' -> { pushReset(); 0x0000AA }
+                            '2' -> { pushReset(); 0x00AA00 }
+                            '3' -> { pushReset(); 0x00AAAA }
+                            '4' -> { pushReset(); 0xAA0000 }
+                            '5' -> { pushReset(); 0xAA00AA }
+                            '6' -> { pushReset(); 0xFFAA00 }
+                            '7' -> { pushReset(); 0xAAAAAA }
+                            '8' -> { pushReset(); 0x555555 }
+                            '9' -> { pushReset(); 0x5555FF }
+                            'a' -> { pushReset(); 0x55FF55 }
+                            'b' -> { pushReset(); 0x55FFFF }
+                            'c' -> { pushReset(); 0xFF5555 }
+                            'd' -> { pushReset(); 0xFF55FF }
+                            'e' -> { pushReset(); 0xFFFF55 }
+                            'f' -> { pushReset(); 0xFFFFFF }
+                            else -> {
+                                pushPart() // We're about to change style, push the last part with the old style.
+
+                                // Oops, we don't actually want to change the colour.
+                                // Handle styles and rainbows here.
+                                // Leave your pot of gold at the door.
+                                when (next) {
+                                    'k' -> obfuscated = true
+                                    'l' -> bold = true
+                                    'm' -> strike = true
+                                    'n' -> underline = true
+                                    'o' -> italic = true
+                                    'r' -> pushReset()
+                                    else -> rainbow = true
+                                }
+
+                                colour
+                            }
+                        }
+                    }
+                    '#' -> { // Hex colour
+                        pushPart()
+
+                        var colourBuf = ""
+                        loop@ while (iterator.hasNext()) {
+                            colourBuf += when (val colourC = iterator.nextChar()) {
+                                in '0'..'9' -> colourC
+                                in 'a'..'f' -> colourC
+                                else -> {
+                                    if (colourC != '\\') {
+                                        buf += colourC
+                                    }
+                                    break@loop
+                                }
+                            }
+                        }
+
+                        when (colourBuf.length) {
+                            6 -> { // no alpha (format is #rrggbb[aa]
+                                colour = Integer.decode("0x$colourBuf")
+                            }
+                            8 -> {
+                                colour = Integer.decode("0x${colourBuf.substring(0..5)}")
+                                alpha = Integer.decode("0x${colourBuf.substring(6)}")
+                            }
+                            else -> {
+                                // Invalid format! Ignore this colour.
+                            }
+                        }
+                    }
+                    '$' -> { // Variable
+                        pushPart()
+                        var varBuf = ""
+                        loop@ while (iterator.hasNext()) {
+                            varBuf += when (val varC = iterator.nextChar()) {
+                                in 'a'..'z' -> varC
+                                else -> {
+                                    if (varC != '\\') {
+                                        buf += varC
+                                    }
+                                    break@loop
+                                }
+                            }
+                        }
+
+                        val supplier = when (varBuf) {
+                            "x" -> Supplier { Wrapper.getMinecraft().player.x.toString() }
+                            "y" -> Supplier { Wrapper.getMinecraft().player.y.toString() }
+                            "z" -> Supplier { Wrapper.getMinecraft().player.z.toString() }
+                            "yaw" -> Supplier { Wrapper.getMinecraft().player.yaw.toString() }
+                            "pitch" -> Supplier { Wrapper.getMinecraft().player.pitch.toString() }
+                            "tps" -> Supplier { LagCompensator.INSTANCE.tickRate.toString() }
+                            "username" -> Supplier { Wrapper.getMinecraft().session.username }
+                            // TODO: gametime, realtime, gameticks, anticheat
+                            else -> Supplier { "unknown variable" }
+                        }
+                        pushPart(supplier)
+                    }
+                    '\\' -> { // Escape
+                        if (escape) {
+                            // escaping an escape
+                            buf += '\\'
+                        }
+                        escape = true
+                    }
+                    else -> {
+                        if (escape) {
+                            // We've tried to escape a non-escapable character.
+                            // Just toss the backslash and this character onto the buffer.
+                            buf += '\\'
+                            escape = false
+                        }
+                        buf += c
+                    }
+                }
+            }
+
+            pushPart() // push remainder
+            return parts
         }
         
-        override fun toString(): String {
-            var str = ""
-            for (supplier in parts) {
-                str += supplier.get()
-            }
-            return str
-        }
+        data class Cmd(
+            val supplier: Supplier<String>,
+            val color: Int,
+            val obfuscated: Boolean = false,
+            val bold: Boolean = false,
+            val strike: Boolean = false,
+            val underline: Boolean = false,
+            val italic: Boolean = false,
+            val rainbow: Boolean = false,
+            val alpha: Int = 0xFF
+        )
     }
+
 }
