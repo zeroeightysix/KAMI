dd49afbd8947a75d2c17e499187698f8520ed7e2
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-07-07 20:05:03 +0200

    Fix non-nullable CloseScreenInPortalEvent#screen Had to be nullable

--- src/main/kotlin/me/zeroeightsix/kami/event/events/CloseScreenInPortalEvent.kt
+++ src/main/kotlin/me/zeroeightsix/kami/event/events/CloseScreenInPortalEvent.kt
@@ -3,4 +3,4 @@ package me.zeroeightsix.kami.event.events
 import me.zeroeightsix.kami.event.KamiEvent
 import net.minecraft.client.gui.screen.Screen
 
-class CloseScreenInPortalEvent(val screen: Screen) : KamiEvent()
+class CloseScreenInPortalEvent(val screen: Screen?) : KamiEvent()
