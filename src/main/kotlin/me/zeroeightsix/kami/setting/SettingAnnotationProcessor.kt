e03162c4a783d137a6ce2a9bab42f1b3f168446b
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-07-24 23:32:22 +0200

    Create custom setting interfaces for enum types

--- src/main/kotlin/me/zeroeightsix/kami/setting/SettingAnnotationProcessor.kt
+++ src/main/kotlin/me/zeroeightsix/kami/setting/SettingAnnotationProcessor.kt
@@ -1,5 +1,6 @@
 package me.zeroeightsix.kami.setting
 
+import imgui.ImGui
 import io.github.fablabsmc.fablabs.api.fiber.v1.FiberId
 import io.github.fablabsmc.fablabs.api.fiber.v1.annotation.Setting
 import io.github.fablabsmc.fablabs.api.fiber.v1.annotation.processor.LeafAnnotationProcessor
@@ -7,6 +8,7 @@ import io.github.fablabsmc.fablabs.api.fiber.v1.builder.ConfigLeafBuilder
 import io.github.fablabsmc.fablabs.api.fiber.v1.schema.type.derived.ConfigTypes
 import io.github.fablabsmc.fablabs.api.fiber.v1.schema.type.derived.StringConfigType
 import io.github.fablabsmc.fablabs.api.fiber.v1.tree.ConfigAttribute
+import me.zeroeightsix.kami.setting.KamiConfig.createInterface
 import me.zeroeightsix.kami.setting.KamiConfig.typeMap
 import net.minecraft.util.Identifier
 import java.lang.reflect.Field
@@ -27,10 +29,35 @@ object SettingAnnotationProcessor : LeafAnnotationProcessor<Setting> {
             ConfigAttribute.create(
                 FiberId("kami", "setting_interface"),
                 INTERFACE_TYPE,
-                typeMap.getOrDefault(
-                    field!!.type,
-                    typeMap.getOrDefault(builder.type.erasedPlatformType, SettingInterface.Default)
-                )
+                if (field!!.type.isEnum) {
+                    // Create a custom interface for this enum
+                    val values = field.type.enumConstants
+                    val strings = values.map { it.toString() }
+                    val interf = createInterface(
+                        {
+                            Pair("enum", it.value.toString())
+                        }, {
+                            values.find { e -> it.equals(e.toString(), ignoreCase = true) }
+                        },
+                        { leaf ->
+                            with(ImGui) {
+                                val current = IntArray(1) { strings.indexOf(leaf.value as String) }
+                                combo(leaf.name, current, strings)
+                                leaf.value = strings.getOrElse(current[0]) { values[0] }
+                            }
+                        },
+                        {
+                            it.buildFuture()
+                        }, "enum-${field.type.simpleName.toLowerCase()}"
+                    )
+                    SettingInterface.Registry.add(interf.id, interf)
+                    interf
+                } else {
+                    typeMap.getOrDefault(
+                        field.type,
+                        typeMap.getOrDefault(builder.type.erasedPlatformType, SettingInterface.Default)
+                    )
+                }
             )
         )
     }
