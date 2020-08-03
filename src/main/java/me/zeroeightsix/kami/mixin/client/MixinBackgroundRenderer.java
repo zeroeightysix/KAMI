9af3f95d6ff0efe97ec4baa63218dc2c2014d3cc
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-03 10:10:43 +0200

    Fix AntiFog

---
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinBackgroundRenderer.java
@@ -0,0 +1,22 @@
+package me.zeroeightsix.kami.mixin.client;
+
+import me.zeroeightsix.kami.KamiMod;
+import me.zeroeightsix.kami.event.events.ApplyFogEvent;
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BackgroundRenderer.class)
+public class MixinBackgroundRenderer {
+
+    @Inject(method = "applyFog", at = @At("HEAD"), cancellable = true)
+    private static void onApplyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, CallbackInfo ci) {
+        ApplyFogEvent event = new ApplyFogEvent();
+        KamiMod.EVENT_BUS.post(event);
+        if (event.isCancelled()) ci.cancel();
+    }
+
+}
