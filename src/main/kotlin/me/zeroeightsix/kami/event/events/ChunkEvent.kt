63b53d4dd4a94bb59e725e95a8bcea9dd427b5d6
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-10 12:25:30 +0200

    Fix ChunkEvent.Unload: chunk is nullable

--- src/main/kotlin/me/zeroeightsix/kami/event/events/ChunkEvent.kt
+++ src/main/kotlin/me/zeroeightsix/kami/event/events/ChunkEvent.kt
@@ -7,7 +7,7 @@ import net.minecraft.world.chunk.Chunk
 /**
  * @author 086
  */
-open class ChunkEvent private constructor(val chunk: Chunk) : KamiEvent() {
+open class ChunkEvent private constructor(val chunk: Chunk?) : KamiEvent() {
     class Load(chunk: Chunk, val packet: ChunkDataS2CPacket) : ChunkEvent(chunk)
-    class Unload(chunk: Chunk) : ChunkEvent(chunk)
+    class Unload(chunk: Chunk?) : ChunkEvent(chunk)
 }
