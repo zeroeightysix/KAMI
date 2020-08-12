ce3c019efdb1105e5d3f5b31c4d29ace6c1bf632
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-12 16:04:28 +0200

    Update changed locals in MixinNetHandlerPlayClient

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinNetHandlerPlayClient.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinNetHandlerPlayClient.java
@@ -4,6 +4,7 @@ import me.zeroeightsix.kami.KamiMod;
 import me.zeroeightsix.kami.event.events.ChunkEvent;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
+import net.minecraft.world.biome.source.BiomeArray;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +21,7 @@ public class MixinNetHandlerPlayClient {
     @Inject(method = "onChunkData",
             at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/world/ClientChunkManager;loadChunkFromPacket(IILnet/minecraft/world/biome/source/BiomeArray;Lnet/minecraft/network/PacketByteBuf;Lnet/minecraft/nbt/CompoundTag;IZ)Lnet/minecraft/world/chunk/WorldChunk;"),
             locals = LocalCapture.CAPTURE_FAILHARD)
-    private void read(ChunkDataS2CPacket packet, CallbackInfo ci, int i, int j, WorldChunk worldChunk) {
+    private void read(ChunkDataS2CPacket packet, CallbackInfo ci, int i, int j, BiomeArray biomeArray, WorldChunk worldChunk) {
         KamiMod.EVENT_BUS.post(new ChunkEvent.Load(worldChunk, packet));
     }
 
