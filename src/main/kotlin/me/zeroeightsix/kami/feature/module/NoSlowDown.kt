4562f1b35593af40a0e0b38517654435b2f16224
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-14 00:30:59 +0200

    Kotlinise NoSlowDown

--- src/main/kotlin/me/zeroeightsix/kami/feature/module/NoSlowDown.kt
+++ src/main/kotlin/me/zeroeightsix/kami/feature/module/NoSlowDown.kt
@@ -1,35 +1,41 @@
-package me.zeroeightsix.kami.feature.module;
+package me.zeroeightsix.kami.feature.module
 
-import me.zero.alpine.listener.EventHandler;
-import me.zero.alpine.listener.Listener;
-import me.zeroeightsix.kami.event.EntityVelocityMultiplierEvent;
-import me.zeroeightsix.kami.event.InputUpdateEvent;
+import me.zero.alpine.listener.EventHandler
+import me.zero.alpine.listener.EventHook
+import me.zero.alpine.listener.Listener
+import me.zeroeightsix.kami.event.EntityVelocityMultiplierEvent
+import me.zeroeightsix.kami.event.InputUpdateEvent
 
 /**
  * Created by 086 on 15/12/2017.
  */
-@Module.Info(name = "NoSlowDown", category = Module.Category.MOVEMENT)
-public class NoSlowDown extends Module {
+@Module.Info(
+    name = "NoSlowDown",
+    category = Module.Category.MOVEMENT
+)
+object NoSlowDown : Module() {
+    @EventHandler
+    private val inputUpdateEventListener =
+        Listener(
+            EventHook { event: InputUpdateEvent ->
+                //
+                // InputUpdateEvent is called just before the player is slowed down @see EntityPlayerSP.onLivingUpdate)
+                // We'll abuse this fact, and multiply moveStrafe and moveForward by 5 to nullify the *0.2f hardcoded by mojang.
+                //
+
+                // Check if the player should be slowed down or not
+                if (mc.player!!.isUsingItem && !mc.player!!.isRiding) {
+                    event.newState.movementSideways *= 5f
+                    event.newState.movementForward *= 5f
+                }
+            }
+        )
 
     @EventHandler
-    private Listener<InputUpdateEvent> inputUpdateEventListener = new Listener<>(event -> {
-        //
-        // InputUpdateEvent is called just before the player is slowed down @see EntityPlayerSP.onLivingUpdate)
-        // We'll abuse this fact, and multiply moveStrafe and moveForward by 5 to nullify the *0.2f hardcoded by mojang.
-        //
-
-        // Check if the player should be slowed down or not
-        if (mc.player.isUsingItem() && !mc.player.isRiding()) {
-            event.getNewState().movementSideways *= 5;
-            event.getNewState().movementForward *= 5;
-        }
-    });
-
-    @EventHandler
-    private Listener<EntityVelocityMultiplierEvent> entityVelocityMultiplierEventListener = new Listener<>(event -> {
-        if (event.getEntity() == mc.player) event.setMultiplier(1f);
-    });
-
-    // Check MixinBlockSoulSand for soulsand slowdown nullification
-
+    private val entityVelocityMultiplierEventListener =
+        Listener(
+            EventHook { event: EntityVelocityMultiplierEvent ->
+                if (event.entity === mc.player) event.multiplier = 1f
+            }
+        )
 }
