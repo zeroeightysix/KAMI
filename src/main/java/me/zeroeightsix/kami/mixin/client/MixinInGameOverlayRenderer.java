7ef479aca20432b64f6021ba0f333503e0ab6fb0
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-19 23:25:27 +0200

    Fixed MixinFirstPersonRenderer: `renderFireOverlay` was moved to InGameOverlayRenderer

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinInGameOverlayRenderer.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinInGameOverlayRenderer.java
@@ -1,17 +1,17 @@
 package me.zeroeightsix.kami.mixin.client;
 
 import me.zeroeightsix.kami.feature.module.NoRender;
-import net.minecraft.client.render.FirstPersonRenderer;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(FirstPersonRenderer.class)
-public class MixinFirstPersonRenderer {
+@Mixin(InGameOverlayRenderer.class)
+public class MixinInGameOverlayRenderer {
     @Inject(method = "renderFireOverlay", at = @At("HEAD"), cancellable = true)
-    private void renderFireOverlay(CallbackInfo ci) {
-        if (NoRender.INSTANCE.isEnabled() && NoRender.INSTANCE.getFire()) {
+    private static void renderFireOverlay(CallbackInfo ci) {
+        if (NoRender.INSTANCE.getEnabled() && NoRender.INSTANCE.getFire()) {
             ci.cancel();
         }
     }
