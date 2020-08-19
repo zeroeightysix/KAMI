b37990c43c46ea280d5284110bc98c589414a227
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-19 19:31:37 +0200

    Show View menu in the void context menu

--- src/main/kotlin/me/zeroeightsix/kami/gui/MenuBar.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/MenuBar.kt
@@ -29,6 +29,23 @@ object View {
 
     @Setting
     var demoWindowVisible = false
+
+    operator fun invoke() = menu("View") {
+        menuItem("Settings", "", selected = Settings.settingsWindowOpen) {
+            Settings.settingsWindowOpen = !Settings.settingsWindowOpen
+        }
+        menuItem("Modules", "", selected = modulesOpen) {
+            modulesOpen = !modulesOpen
+        }
+        menuItem("Module window editor", "", selected = ModuleWindowsEditor.open) {
+            ModuleWindowsEditor.open = !ModuleWindowsEditor.open
+        }
+        if (Settings.demoWindowVisible) {
+            menuItem("Demo window", "", selected = demoWindowVisible) {
+                demoWindowVisible = !demoWindowVisible
+            }
+        }
+    }
 }
 
 object MenuBar {
@@ -37,22 +54,7 @@ object MenuBar {
 
     operator fun invoke() = mainMenuBar {
         EnabledWidgets()
-        menu("View") {
-            menuItem("Settings", "", selected = Settings.settingsWindowOpen) {
-                Settings.settingsWindowOpen = !Settings.settingsWindowOpen
-            }
-            menuItem("Modules", "", selected = modulesOpen) {
-                modulesOpen = !modulesOpen
-            }
-            menuItem("Module window editor", "", selected = ModuleWindowsEditor.open) {
-                ModuleWindowsEditor.open = !ModuleWindowsEditor.open
-            }
-            if (Settings.demoWindowVisible) {
-                menuItem("Demo window", "", selected = demoWindowVisible) {
-                    demoWindowVisible = !demoWindowVisible
-                }
-            }
-        }
+        View()
 
         // imgui needs a window to add the void popup to, so we disgustingly add it to the only window that will always be there: the main menu bar
         var openModal = false
@@ -65,6 +67,7 @@ object MenuBar {
                     openModal = true
                 }
             }
+            View()
         }
         if (openModal) {
             ImGui.openPopup("Create text widget")
