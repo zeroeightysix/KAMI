20c61c668e52ed19deb748f5b6f067118d7733a3
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-19 18:52:06 +0200

    Add Create>Text widget to void context menu Allows user to define new pinnable text widgets.

--- src/main/kotlin/me/zeroeightsix/kami/gui/MenuBar.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/MenuBar.kt
@@ -1,13 +1,22 @@
 package me.zeroeightsix.kami.gui
 
+import glm_.vec2.Vec2
+import glm_.vec4.Vec4
+import imgui.Col
+import imgui.ImGui
 import imgui.MouseButton
+import imgui.WindowFlag
+import imgui.dsl.button
 import imgui.dsl.mainMenuBar
 import imgui.dsl.menu
 import imgui.dsl.menuItem
 import imgui.dsl.popupContextVoid
+import imgui.dsl.popupModal
+import me.zeroeightsix.kami.backToString
 import me.zeroeightsix.kami.gui.View.demoWindowVisible
 import me.zeroeightsix.kami.gui.View.modulesOpen
 import me.zeroeightsix.kami.gui.widgets.EnabledWidgets
+import me.zeroeightsix.kami.gui.widgets.TextPinnableWidget
 import me.zeroeightsix.kami.gui.windows.Settings
 import me.zeroeightsix.kami.gui.windows.modules.ModuleWindowsEditor
 import me.zeroeightsix.kami.gui.windows.modules.Modules
@@ -18,6 +27,8 @@ object View {
 }
 
 object MenuBar {
+
+    private var buffer = ByteArray(128)
 
     operator fun invoke() = mainMenuBar {
         EnabledWidgets()
@@ -39,9 +50,37 @@ object MenuBar {
         }
 
         // imgui needs a window to add the void popup to, so we disgustingly add it to the only window that will always be there: the main menu bar
+        var openModal = false
         popupContextVoid("kami-void-popup", MouseButton.Right) {
             menuItem("Resize module windows") {
                 Modules.resize = true
+            }
+            menu("Create") {
+                menuItem("Text widget") {
+                    openModal = true
+                }
+            }
+        }
+        if (openModal) {
+            ImGui.openPopup("Create text widget")
+        }
+
+        popupModal("Create text widget", extraFlags = WindowFlag.AlwaysAutoResize.i) {
+            ImGui.inputText("Title", buffer)
+
+            ImGui.pushStyleColor(Col.Text, Vec4(.7f, .7f, .7f, 1f))
+            button("Cancel", Vec2(100, 0)) {
+                ImGui.closeCurrentPopup()
+            }
+            ImGui.popStyleColor()
+            ImGui.sameLine()
+            button("Create", Vec2(100, 0)) {
+                val title = buffer.backToString()
+                buffer = ByteArray(128)
+                EnabledWidgets.widgets.add(
+                    TextPinnableWidget(title, mutableListOf())
+                )
+                ImGui.closeCurrentPopup()
             }
         }
     }
