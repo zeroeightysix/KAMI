f3e8f2bd413e6ea684b4220854500717aa353372
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-27 00:38:55 +0200

    Passive test: check for PassiveEntity instead of AnimalEntity AnimalEntity extends PassiveEntity. Now the check also includes villagers.

--- src/main/kotlin/me/zeroeightsix/kami/util/Targets.kt
+++ src/main/kotlin/me/zeroeightsix/kami/util/Targets.kt
@@ -11,8 +11,8 @@ import net.minecraft.entity.mob.AmbientEntity
 import net.minecraft.entity.mob.Angerable
 import net.minecraft.entity.mob.HostileEntity
 import net.minecraft.entity.mob.WaterCreatureEntity
-import net.minecraft.entity.passive.AnimalEntity
 import net.minecraft.entity.passive.IronGolemEntity
+import net.minecraft.entity.passive.PassiveEntity
 import net.minecraft.entity.passive.WolfEntity
 import net.minecraft.entity.player.PlayerEntity
 
@@ -24,7 +24,7 @@ val invalidationListener = Listener<TickEvent.Client.InGame>({
 
 fun isPassive(e: Entity): Boolean {
     if (e is WolfEntity && e.angryAt != mc.player?.uuid) return false
-    return if (e is AnimalEntity || e is AmbientEntity || e is WaterCreatureEntity) true else e is IronGolemEntity && e.target === null
+    return if (e is PassiveEntity || e is AmbientEntity || e is WaterCreatureEntity) true else e is IronGolemEntity && e.target === null
 }
 
 fun isHostile(e: Entity) = e is HostileEntity || (e is Angerable && e.angryAt == mc.player?.uuid)
