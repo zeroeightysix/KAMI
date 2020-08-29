4854511a3c674a74959b5ce264c0e27cf5051e05
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-08-29 21:51:33 +0200

    Fix MixinRenderLiving naming

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinLivingEntityRenderer.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinLivingEntityRenderer.java
@@ -1,21 +1,32 @@
 package me.zeroeightsix.kami.mixin.client;
 
 import me.zeroeightsix.kami.feature.module.Chams;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntityRenderer.class)
-public class MixinRenderLiving {
+public abstract class MixinLivingEntityRenderer<T extends LivingEntity> extends EntityRenderer<T> {
+
+    protected MixinLivingEntityRenderer(EntityRenderDispatcher dispatcher) {
+        super(dispatcher);
+    }
+
+    @Shadow
+    protected abstract RenderLayer getRenderLayer(T entity, boolean showBody, boolean translucent, boolean showOutline);
 
     @Inject(method = "render", at = @At("HEAD"))
-    private <T extends LivingEntity> void injectChamsPre(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+    private void injectChamsPre(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         if (Chams.INSTANCE.getEnabled() && Chams.INSTANCE.renderChams(livingEntity)) {
             GL11.glEnable(32823);
             GL11.glPolygonOffset(1.0f, -1000000.0f);
@@ -23,7 +34,7 @@ public class MixinRenderLiving {
     }
 
     @Inject(method = "render", at = @At("RETURN"))
-    private <T extends LivingEntity> void injectChamsPost(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+    private void injectChamsPost(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         if (Chams.INSTANCE.getEnabled() && Chams.INSTANCE.renderChams(livingEntity)) {
             GL11.glPolygonOffset(1.0f, 1000000.0f);
             GL11.glDisable(32823);
