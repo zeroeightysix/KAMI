4810cdaffbbc941b16f23b842e84d7baf391e7e9
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-09-04 16:10:20 +0200

    Use the new spoofer for Flight And added an isDead check to packet mode: no need to fly when you're dead

--- src/main/java/me/zeroeightsix/kami/feature/module/movement/Flight.java
+++ src/main/java/me/zeroeightsix/kami/feature/module/movement/Flight.java
@@ -4,10 +4,10 @@ import io.github.fablabsmc.fablabs.api.fiber.v1.annotation.Setting;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.event.TickEvent;
+import me.zeroeightsix.kami.feature.hidden.PlayerMovementSpoofer;
 import me.zeroeightsix.kami.feature.module.Module;
 import me.zeroeightsix.kami.util.EntityUtil;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.math.Vec3d;
 
 @Module.Info(category = Module.Category.MOVEMENT, description = "Makes the player fly", name = "Flight")
@@ -40,6 +40,8 @@ public class Flight extends Module {
                 mc.player.abilities.allowFlying = true;
                 break;
             case PACKET:
+                if (mc.player.isDead()) return;
+
                 int angle;
 
                 boolean forward = mc.options.keyForward.isPressed();
@@ -61,8 +63,13 @@ public class Flight extends Module {
                 }
 
                 EntityUtil.updateVelocityY(mc.player, 0);
-                mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.Both(mc.player.getX() + mc.player.getVelocity().x, mc.player.getY() + (MinecraftClient.getInstance().options.keyJump.isPressed() ? 0.0622 : 0) - (MinecraftClient.getInstance().options.keySneak.isPressed() ? 0.0622 : 0), mc.player.getZ() + mc.player.getVelocity().z, mc.player.yaw, mc.player.pitch, false));
-                mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.Both(mc.player.getX() + mc.player.getVelocity().x, mc.player.getY() - 42069 /* nice */, mc.player.getZ() + mc.player.getVelocity().z, mc.player.yaw, mc.player.pitch, true));
+
+                PlayerMovementSpoofer.INSTANCE.setPosition(new Vec3d(mc.player.getX() + mc.player.getVelocity().x, mc.player.getY() + (MinecraftClient.getInstance().options.keyJump.isPressed() ? 0.0622 : 0) - (MinecraftClient.getInstance().options.keySneak.isPressed() ? 0.0622 : 0), mc.player.getZ() + mc.player.getVelocity().z),
+                        PlayerMovementSpoofer.Priority.NORMAL,
+                        PlayerMovementSpoofer.Mode.INSTANT);
+                PlayerMovementSpoofer.INSTANCE.setPosition(new Vec3d(mc.player.getX() + mc.player.getVelocity().x, mc.player.getY() - 42069 /* nice */, mc.player.getZ() + mc.player.getVelocity().z),
+                        PlayerMovementSpoofer.Priority.NORMAL,
+                        PlayerMovementSpoofer.Mode.INSTANT);
                 break;
         }
     });
