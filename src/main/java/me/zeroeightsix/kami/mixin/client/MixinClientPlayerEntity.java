16a86bce9edcdb2d84be7c6146256685b2f53851
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-09-10 13:24:59 +0200

    Fix NoSlowDown's cancelling of item use slowdown

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinClientPlayerEntity.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinClientPlayerEntity.java
@@ -1,21 +1,44 @@
 package me.zeroeightsix.kami.mixin.client;
 
+import com.mojang.authlib.GameProfile;
 import me.zeroeightsix.kami.KamiMod;
 import me.zeroeightsix.kami.event.CloseScreenInPortalEvent;
 import me.zeroeightsix.kami.event.PlayerMoveEvent;
+import me.zeroeightsix.kami.feature.module.NoSlowDown;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.input.Input;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.MovementType;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
-public class MixinClientPlayerEntity {
+public abstract class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
+
+    @Shadow
+    public Input input;
+
+    public MixinClientPlayerEntity(ClientWorld world, GameProfile profile) {
+        super(world, profile);
+    }
+
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/input/Input;tick(Z)V", shift = At.Shift.AFTER))
+    public void onTickMovement(CallbackInfo ci) {
+        // Cancel item use slowdown
+        if (NoSlowDown.INSTANCE.getEnabled() && this.isUsingItem() && !this.hasVehicle()) {
+            Input var10000 = this.input;
+            var10000.movementForward /= 0.2F;
+            var10000.movementSideways /= 0.2F;
+        }
+    }
 
     @Redirect(method = "updateNausea", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;openScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
     public void openScreen(MinecraftClient client, Screen screen) {
@@ -25,6 +48,9 @@ public class MixinClientPlayerEntity {
             client.openScreen(screen);
         }
     }
+
+    @Shadow
+    public abstract boolean isUsingItem();
 
     @Inject(method = "move", cancellable = true, at = @At("HEAD"))
     public void move(MovementType type, Vec3d vec, CallbackInfo info) {
--- src/main/kotlin/me/zeroeightsix/kami/feature/module/NoSlowDown.kt
+++ src/main/kotlin/me/zeroeightsix/kami/feature/module/NoSlowDown.kt
@@ -3,30 +3,12 @@ package me.zeroeightsix.kami.feature.module
 import me.zero.alpine.listener.EventHandler
 import me.zero.alpine.listener.Listener
 import me.zeroeightsix.kami.event.EntityVelocityMultiplierEvent
-import me.zeroeightsix.kami.event.InputUpdateEvent
 
 @Module.Info(
     name = "NoSlowDown",
     category = Module.Category.MOVEMENT
 )
 object NoSlowDown : Module() {
-    @EventHandler
-    private val inputUpdateEventListener =
-        Listener(
-            { event: InputUpdateEvent ->
-                //
-                // InputUpdateEvent is called just before the player is slowed down @see EntityPlayerSP.onLivingUpdate)
-                // We'll abuse this fact, and multiply moveStrafe and moveForward by 5 to nullify the *0.2f hardcoded by mojang.
-                //
-
-                // Check if the player should be slowed down or not
-                if (mc.player!!.isUsingItem && !mc.player!!.isRiding) {
-                    event.newState.movementSideways *= 5f
-                    event.newState.movementForward *= 5f
-                }
-            }
-        )
-
     @EventHandler
     private val entityVelocityMultiplierEventListener =
         Listener(
