8bf5d85a272c799ecd6a29e05b697f80c3b9aeee
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-09-13 16:58:13 +0200

    Move VarMap and CompiledText to new `text` package in gui

--- src/main/kotlin/me/zeroeightsix/kami/gui/widgets/TextPinnableWidget.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/widgets/TextPinnableWidget.kt
@@ -8,10 +8,8 @@ import imgui.ImGui.calcTextSize
 import imgui.ImGui.colorEditVec4
 import imgui.ImGui.currentWindow
 import imgui.ImGui.cursorPosX
-import imgui.ImGui.dragInt
 import imgui.ImGui.dummy
 import imgui.ImGui.getMouseDragDelta
-import imgui.ImGui.inputText
 import imgui.ImGui.isItemActive
 import imgui.ImGui.isItemHovered
 import imgui.ImGui.openPopup
@@ -36,10 +34,10 @@ import imgui.dsl.window
 import me.zeroeightsix.kami.*
 import me.zeroeightsix.kami.gui.KamiGuiScreen
 import me.zeroeightsix.kami.gui.KamiHud
+import me.zeroeightsix.kami.gui.text.CompiledText
+import me.zeroeightsix.kami.gui.text.VarMap
 import me.zeroeightsix.kami.util.ResettableLazy
 import kotlin.collections.map
-import kotlin.math.abs
-import kotlin.math.floor
 
 open class TextPinnableWidget(
     val title: String,
@@ -73,10 +71,6 @@ open class TextPinnableWidget(
         }
     }
     private val immediateText by immediateTextDelegate
-
-    companion object {
-
-    }
 
     override fun fillWindow() {
         val guiOpen = mc.currentScreen is KamiGuiScreen
@@ -420,244 +414,6 @@ open class TextPinnableWidget(
     override fun fillContextMenu() {
         menuItem("Edit") {
             editWindow = true
-        }
-    }
-
-    class CompiledText(
-        var parts: MutableList<Part> = mutableListOf()
-    ) {
-
-        override fun toString(): String {
-            var buf = ""
-            for (part in parts) buf += part.toString()
-            return buf
-        }
-
-        abstract class Part(
-            var obfuscated: Boolean = false,
-            // _names because they have mirrors with custom setters in the class
-            _bold: Boolean = false,
-            _strike: Boolean = false,
-            _underline: Boolean = false,
-            _italic: Boolean = false,
-            var shadow: Boolean = true,
-            var colourMode: ColourMode = ColourMode.STATIC,
-            var extraspace: Boolean = true
-        ) {
-            private fun toCodes(): String {
-                return (if (obfuscated) "§k" else "") +
-                        (if (bold) "§l" else "") +
-                        (if (strike) "§m" else "") +
-                        (if (underline) "§n" else "") +
-                        (if (italic) "§o" else "")
-            }
-
-            open val editLabel: String
-                get() = toString()
-            abstract val multiline: Boolean
-            var codes: String = toCodes()
-
-            var bold: Boolean = _bold
-                set(value) {
-                    field = value
-                    codes = toCodes()
-                }
-            var strike: Boolean = _strike
-                set(value) {
-                    field = value
-                    codes = toCodes()
-                }
-            var underline: Boolean = _underline
-                set(value) {
-                    field = value
-                    codes = toCodes()
-                }
-            var italic: Boolean = _italic
-                set(value) {
-                    field = value
-                    codes = toCodes()
-                }
-
-            private fun Vec4.toARGB(): Int {
-                val r = (x * 255.0F).toInt()
-                val g = (y * 255.0F).toInt()
-                val b = (z * 255.0F).toInt()
-                val a = (w * 255.0F).toInt()
-                return (a shl 24) or (r shl 16) or (g shl 8) or b
-            }
-
-            // Static colour
-            var colour: Vec4 = Vec4(1.0f, 1.0f, 1.0f, 1.0f)
-                set(value) {
-                    field = value
-                    argb = colour.toARGB()
-                }
-            private var argb: Int = this.colour.toARGB()
-
-            // Alternating colour
-            var colours = mutableListOf(
-                Vec4(1.0f, 1.0f, 1.0f, 1.0f),
-                Vec4(0.5f, 0.5f, 0.5f, 1.0f)
-            )
-            val coloursIterator = colours.cyclingIterator()
-
-            override fun toString(): String {
-                return if (extraspace) " " else ""
-            }
-
-            fun currentColour(): Vec4 {
-                return when (colourMode) {
-                    ColourMode.STATIC -> colour
-                    ColourMode.RAINBOW -> KamiMod.rainbow.vec4
-                    ColourMode.ALTERNATING -> coloursIterator.next()
-                }
-            }
-
-            fun currentColourARGB(): Int {
-                return currentColour().toARGB()
-            }
-
-            /**
-             * For parts that might have a coloured pattern; this method must be called when you're done drawing that part.
-             */
-            fun resetMultilinePattern() {
-                coloursIterator.reset()
-            }
-
-            enum class ColourMode(private val multilineExclusive: Boolean = false) {
-                STATIC, RAINBOW, ALTERNATING(true);
-
-                companion object {
-                    val listNormal = values().filter { !it.multilineExclusive }.joinToString("$NUL") { it.name.toLowerCase().capitalize() }
-                    val listMultiline = values().joinToString("$NUL") { it.name.toLowerCase().capitalize() }
-                }
-            }
-
-            abstract fun edit(variableMap: Map<String, () -> Variable>)
-        }
-
-        class LiteralPart(
-            var string: String,
-            obfuscated: Boolean = false,
-            bold: Boolean = false,
-            strike: Boolean = false,
-            underline: Boolean = false,
-            italic: Boolean = false,
-            shadow: Boolean = true,
-            colourMode: ColourMode = ColourMode.STATIC,
-            extraspace: Boolean = true
-        ) : Part(obfuscated, bold, strike, underline, italic, shadow, colourMode, extraspace) {
-            override val multiline = false
-
-            override fun toString(): String {
-                return string + super.toString()
-            }
-
-            override fun edit(variableMap: Map<String, () -> Variable>) {
-                val buf = string.toByteArray(ByteArray((((floor((abs((string.length - 4) / 256) + 1).toDouble()))) * 256).toInt()))
-                if (inputText("Text", buf)) {
-                    string = buf.backToString()
-                }
-                sameLine()
-                text("+")
-                sameLine()
-                val space = booleanArrayOf(extraspace)
-                if (ImGui.checkbox("Space", space)) {
-                    extraspace = space[0]
-                }
-            }
-        }
-
-        class VariablePart(
-            var variable: Variable,
-            obfuscated: Boolean = false,
-            bold: Boolean = false,
-            strike: Boolean = false,
-            underline: Boolean = false,
-            italic: Boolean = false,
-            shadow: Boolean = true,
-            colourMode: ColourMode = ColourMode.STATIC,
-            extraspace: Boolean = true
-        ) : Part(obfuscated, bold, strike, underline, italic, shadow, colourMode, extraspace) {
-            private var editVarComboIndex = -1
-            private var editDigits = (variable is NumericalVariable).then(
-                { (variable as NumericalVariable).digits }, { 0 }
-            )
-
-            override val multiline: Boolean
-                get() = variable.multiline
-
-            override val editLabel: String
-                get() = variable.editLabel + super.toString()
-
-            override fun toString(): String {
-                return variable.provide() + super.toString()
-            }
-
-            override fun edit(variableMap: Map<String, () -> Variable>) {
-                if (editVarComboIndex == -1) editVarComboIndex = variableMap.keys.indexOf(this.variable.name)
-                combo("Variable", ::editVarComboIndex, variableMap.keys.joinToString(0.toChar().toString())) {
-                    val selected: String = variableMap.keys.toList()[editVarComboIndex]
-                    val v = (variableMap[selected] ?: error("Invalid item selected")).invoke()
-                    if (v is NumericalVariable) {
-                        v.digits = editDigits
-                    }
-                    this.variable = v
-                }
-                sameLine()
-                text("+")
-                sameLine()
-                val space = booleanArrayOf(extraspace)
-                if (ImGui.checkbox("Space", space)) {
-                    extraspace = space[0]
-                }
-                when (val variable = variable) {
-                    is NumericalVariable -> {
-                        if (dragInt("Digits", ::editDigits, vSpeed = 0.1f, vMin = 0, vMax = 8)) {
-                            variable.digits = editDigits
-                        }
-                    }
-                }
-                variable.edit(variableMap)
-            }
-        }
-
-        abstract class Variable(val name: String) {
-            abstract val multiline: Boolean
-            open val editLabel: String
-                get() = provide()
-
-            abstract fun provide(): String
-            open fun edit(variableMap: Map<String, () -> Variable>) {}
-        }
-
-        class ConstantVariable(name: String, _multiline: Boolean = false, private val string: String) : Variable(name) {
-            override val multiline = _multiline
-
-            override fun provide(): String {
-                return string
-            }
-        }
-
-        class NumericalVariable(name: String, private val provider: () -> Double?, var digits: Int = 0) :
-            Variable(name) {
-            override val multiline = false
-
-            override fun provide(): String {
-                val number = provideNumber()
-                return String.format("%.${digits}f", number)
-            }
-
-            fun provideNumber() = provider()
-        }
-
-        open class StringVariable(name: String, _multiline: Boolean = false, private val provider: () -> String) :
-            Variable(name) {
-            override val multiline = _multiline
-
-            override fun provide(): String {
-                return provider()
-            }
         }
     }
 
