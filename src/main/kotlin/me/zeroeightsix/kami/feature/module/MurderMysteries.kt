b00f8863dad76cd65cc92c04097e28a86a54d347
Humboldt123 <nonemunhsiv@gmail.com>
2020-09-15 12:28:19 -0400

    Refactored Code

--- src/main/kotlin/me/zeroeightsix/kami/feature/module/MurderMysteries.kt
+++ src/main/kotlin/me/zeroeightsix/kami/feature/module/MurderMysteries.kt
@@ -6,7 +6,7 @@ import me.zero.alpine.listener.EventHandler
 import me.zero.alpine.listener.Listener
 import me.zeroeightsix.kami.event.TickEvent
 import me.zeroeightsix.kami.util.Texts
-import net.minecraft.item.Items
+import net.minecraft.item.*
 import net.minecraft.text.MutableText
 import net.minecraft.util.Formatting
 
@@ -25,12 +25,15 @@ object MurderMysteries : Module() {
     private var toolItems = true
     @Setting(name = "DetectiveItems")
     private var detectiveItems = false
+    @Setting(name = "IgnoreWoodenShovel")
+    private var ignoreWoodenShovel = false
 
     @Setting(name = "CooldownOnOutput") // This is how often it will tell the player in chat
-    private var updateEvery: @Constrain.Range(min = 2.0, max = 16.0, step = Double.MIN_VALUE) Int = 16
+    private var updateEvery: @Constrain.Range(min = 2.0, max = 16.0, step = 0.1) Int = 10
 
     private var lastUpdate: Long = 0L
 
+    //I know its deprecated but I am not sure how I should go about this.
     private fun murdererIs(username: String, item: String): MutableText? {
         lastUpdate = System.currentTimeMillis()
         return Texts.f(Formatting.WHITE, Texts.append(
@@ -52,73 +55,46 @@ object MurderMysteries : Module() {
         ))
     }
 
-
     @EventHandler
     val worldListener = Listener<TickEvent.Client.InGame>({
         if ((lastUpdate + updateEvery * 1000L <= System.currentTimeMillis()))
             mc.world?.players?.forEach { player ->
                 val username = player.displayName.string
 
-                //Make sure the player is not being told that they themself are murderer
-                if (username == mc.player?.displayName?.string) return@forEach
+                //Make sure the player is not being told that they are the murderer
+                if (player == mc.player) return@forEach
 
-                //Check for weaponly items (swords, axes)
+                val innocentItemsList = listOf(
+                    Items.CARROT,
+                    Items.BONE,
+                    Items.CARROT_ON_A_STICK,
+                    Items.GOLDEN_CARROT,
+                    Items.BLAZE_ROD,
+                    Items.SPONGE
+                )
+                val detectiveItemsList = listOf(
+                    Items.BOW,
+                    Items.SNOWBALL,
+                    Items.ARROW
+                )
+                //Check for weaponly items (swords, axes)s
                 player.itemsEquipped.forEach { it ->
-                    if (weaponItems) when (it.item) {
-                        Items.NETHERITE_SWORD -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.DIAMOND_SWORD -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.IRON_SWORD -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.GOLDEN_SWORD -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.WOODEN_SWORD -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-
-                        Items.NETHERITE_AXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.DIAMOND_AXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.IRON_AXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.GOLDEN_AXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.WOODEN_AXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                    }
+                    if (weaponItems && (it.item is AxeItem || it.item is SwordItem))
+                        mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
 
                     //Check for tool items like shovels, hoes, and shears, and picks (hypixel knife skin)
-                    if (toolItems) when (it.item) {
-                        Items.NETHERITE_SHOVEL -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.DIAMOND_SHOVEL -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.IRON_SHOVEL -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.GOLDEN_SHOVEL -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.WOODEN_SHOVEL -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-
-                        Items.NETHERITE_HOE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.DIAMOND_HOE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.IRON_HOE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.GOLDEN_HOE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.WOODEN_HOE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-
-                        Items.NETHERITE_PICKAXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.DIAMOND_PICKAXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.IRON_PICKAXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.GOLDEN_PICKAXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.WOODEN_PICKAXE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-
-                        Items.SHEARS -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                    }
+                    if (toolItems && (it.item is ShovelItem || it.item is PickaxeItem || it.item is HoeItem || it.item is ShearsItem) && !(it.item == Items.WOODEN_SHOVEL && ignoreWoodenShovel))
+                        mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
 
                     //Check for otherwise innocent items that could be knife skins on some servers
-                    if (innocuousItems) when (it.item) {
-                        Items.CARROT -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.BONE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.CARROT_ON_A_STICK -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.GOLDEN_CARROT -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.BLAZE_ROD -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                        Items.SPONGE -> mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
-                    }
+                    if (innocuousItems && (it.item in innocentItemsList))
+                        mc.player?.sendMessage(murdererIs(username, it.item.name.string), false)
 
                     //Check for items the detective or innocent players could use to stop them murderer
-                    if (detectiveItems) when (it.item) {
-                        Items.BOW -> mc.player?.sendMessage(detectiveIs(username, it.item.name.string), false)
-                        Items.SNOWBALL -> mc.player?.sendMessage(detectiveIs(username, it.item.name.string), false)
-                        Items.ARROW -> mc.player?.sendMessage(detectiveIs(username, it.item.name.string), false)
-                    }
-                }
+                    if (detectiveItems && it.item in detectiveItemsList)
+                        mc.player?.sendMessage(detectiveIs(username, it.item.name.string), false)
 
+                }
             }
     })
 }
