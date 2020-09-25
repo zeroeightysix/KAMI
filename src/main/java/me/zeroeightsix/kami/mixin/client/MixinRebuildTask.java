88413e27fd9af4861161692f8da53c07994a7725
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-09-25 21:35:41 +0200

    Render to separate framebuffer, render unlit Separate framebuffer means separate depth (so we don't have to e.g. disable depth testing when rendering, which would result in visual bugs with far away objects appearing overlayed over those closeby), and also means that we can apply postprocessing effects. The added quad rendering functions are those from minecraft, but with a lot of unneeded functionality shaved off, including lighting.

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinRebuildTask.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinRebuildTask.java
@@ -1,28 +1,30 @@
 package me.zeroeightsix.kami.mixin.client;
 
 import me.zeroeightsix.kami.world.KamiRenderLayers;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.chunk.BlockBufferBuilderStorage;
 import net.minecraft.client.render.chunk.ChunkBuilder;
 import net.minecraft.client.render.chunk.ChunkOcclusionDataBuilder;
 import net.minecraft.client.render.chunk.ChunkRendererRegion;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Iterator;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 import static org.lwjgl.opengl.GL11C.GL_QUADS;
 
@@ -39,10 +41,42 @@ public class MixinRebuildTask {
                 builder.begin(GL_QUADS, VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL);
             }
 
-            if (blockRenderManager.renderBlock(blockState, blockPos3, chunkRendererRegion, matrixStack, builder, false, random)) {
+            if (renderAllSidesUnlitFlat(blockRenderManager, blockState, blockPos3, chunkRendererRegion, matrixStack, builder, random)) {
                 iChunkData.setEmpty(false);
                 iChunkData.getNonEmptyLayers().add(layer);
             }
+        }
+    }
+
+    private static boolean renderAllSidesUnlitFlat(BlockRenderManager manager, BlockState blockState, BlockPos blockPos, BlockRenderView blockRenderView, MatrixStack matrixStack, VertexConsumer vertexConsumer, Random random) {
+        BlockRenderType blockRenderType = blockState.getRenderType();
+        if (blockRenderType != BlockRenderType.MODEL) return false;
+
+        boolean ret = false;
+
+        Vec3d vec3d = blockState.getModelOffset(blockRenderView, blockPos);
+        matrixStack.translate(vec3d.x, vec3d.y, vec3d.z);
+
+        long seed = blockState.getRenderingSeed(blockPos);
+        BakedModel bakedModel = manager.getModel(blockState);
+        for (Direction direction : Direction.values()) {
+            random.setSeed(seed);
+
+            List<BakedQuad> list = bakedModel.getQuads(blockState, direction, random);
+            if (!list.isEmpty()) {
+                renderQuadsFlatUnlit(manager, blockRenderView, blockState, blockPos, 0xf000f0, OverlayTexture.DEFAULT_UV, matrixStack, vertexConsumer, list, 1.0f);
+                ret = true;
+            }
+        }
+
+        return ret;
+    }
+
+    private static void renderQuadsFlatUnlit(BlockRenderManager manager, BlockRenderView blockRenderView, BlockState blockState, BlockPos blockPos, int i, int j, MatrixStack matrixStack, VertexConsumer vertexConsumer, List<BakedQuad> list, float brightness) {
+        IBlockModelRenderer renderer = (IBlockModelRenderer) manager.getModelRenderer();
+        MatrixStack.Entry model = matrixStack.peek();
+        for (BakedQuad quad : list) {
+            renderer.callRenderQuad(blockRenderView, blockState, blockPos, vertexConsumer, model, quad, brightness, brightness, brightness, brightness, i, i, i, i, j);
         }
     }
 
