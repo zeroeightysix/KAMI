7f67690f2bb0165c80840cc5a90de6bfce784e58
Humboldt123 <nonemunhsiv@gmail.com>
2020-10-12 10:53:34 -0400

    Delete fabic-installer.jar after installing Don't install until you install fabric-installer.jar Detect fabric better (via versions folder) and prompt to install latest fabric if not

--- src/main/java/me/zeroeightsix/installer/Installer.java
+++ src/main/java/me/zeroeightsix/installer/Installer.java
@@ -13,7 +13,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Random;
 
 import org.xml.sax.SAXException;
@@ -76,15 +78,134 @@ public class Installer extends JPanel {
             installButton.setEnabled(false);
             installIcon.setOpaque(false);
             try {
-                installKami(getModsFolder());
-            } catch (IOException | SAXException | ParserConfigurationException e) {
+                installKami(getMinecraftFolder());
+            } catch (IOException | SAXException | ParserConfigurationException | InterruptedException e) {
                 e.printStackTrace();
             }
             System.exit(0);
         });
     }
 
-    private String getModsFolder() {
+    //TODO: Center button or add MultiMC support (I'm leaning towards centering)
+    private void installKami(String directory) throws IOException, SAXException, ParserConfigurationException, InterruptedException {
+        if (!hasLatestFabric(directory.substring(0, directory.length() - 5))) {
+            Process process = installFabric();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            int exitVal = process.waitFor();
+            System.out.println("Exited fabric installation with exit code " + exitVal);
+            reader.close();
+            new File(System.getProperty("user.dir") + File.separator + "fabric-installer.jar").delete();
+        }
+
+        String latestVersion = getLatestKami();
+
+        Path kamiMod = Paths.get(directory + "kami-" + latestVersion + ".jar");
+        InputStream in = new URL("https://github.com/zeroeightysix/KAMI/releases/download/" + latestVersion + "/kami-" + latestVersion + ".jar").openStream();
+
+        Arrays.stream(Objects.requireNonNull(new File(directory).list())).forEach(mod -> {
+            if ((mod.contains("kami-") && !mod.equals("kami-" + latestVersion + ".jar"))) {
+                File modFile = new File(directory + mod);
+                if (!modFile.isDirectory()) {
+                    JOptionPane.showMessageDialog(null, "Outdated KAMI ver. " + mod.substring(0, mod.length() - 4) + " detected. Deleting...");
+                    modFile.delete();
+                }
+            }
+        });
+
+        if (new File(String.valueOf(kamiMod)).exists()) {
+            JOptionPane.showMessageDialog(null, "It looks like KAMI is already installed.");
+        } else {
+            Files.copy(in, kamiMod, StandardCopyOption.REPLACE_EXISTING);
+            JOptionPane.showMessageDialog(null, "Installed KAMI to:\n"+directory);
+        }
+    }
+
+    private Process installFabric() throws IOException, ParserConfigurationException, SAXException {
+        String latestVersion = getLatestFabric();
+        JOptionPane.showMessageDialog(null,"It looks like fabric is not installed, this will prompt you to install fabric version " + latestVersion);
+        Path fabricInstall = Paths.get(System.getProperty("user.dir") + File.separator + "fabric-installer.jar");
+        InputStream in = new URL("https://maven.fabricmc.net/net/fabricmc/fabric-installer/"+latestVersion+"/fabric-installer-"+latestVersion+".jar").openStream();
+        Files.copy(in, fabricInstall, StandardCopyOption.REPLACE_EXISTING);
+        return Runtime.getRuntime().exec("java -jar fabric-installer.jar");
+    }
+
+    /**
+     *  Fetches the latest version of KAMI from releases
+     *  As is the case with the Kami Blue installer,
+     *  because we can't import libraries that minecraft has and because
+     *  the game isn't initialized we **have** to hard code this 😔
+     *
+     * @return The version name of the latest release
+     * @throws IOException
+     */
+    private String getLatestKami() throws IOException {
+        try (InputStream is = new URL("https://api.github.com/repos/zeroeightysix/KAMI/releases").openStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            StringBuilder builder = new StringBuilder();
+
+            int currentCharacter;
+            while ((currentCharacter = reader.read()) != -1) {
+                builder.append((char) currentCharacter);
+            }
+            String jsonText = builder.toString();
+
+            return jsonText.split("tag_name", 2)[1].split("\"", 6)[2];
+
+        }
+    }
+
+    /**
+     *  Checks if there is the latest version of fabric-loader
+     * @param directory The minecraft directory to check for fabric
+     * @return If the directory has the latest version of fabric-loader
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     * @throws IOException
+     */
+    private boolean hasLatestFabric(String directory) throws ParserConfigurationException, SAXException, IOException {
+        String latestFabricLoader = getLatestFabricLoader();
+        for (File file: Objects.requireNonNull(new File(directory + "versions").listFiles())) {
+            String version = new File(String.valueOf(file)).getName();
+            if (version.contains("fabric-loader") && version.contains(latestFabricLoader)) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Fetches the build number of the latest version of fabric
+     * @return The build number of the latest version of fabric
+     * @throws IOException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     */
+    private String getLatestFabric() throws IOException, ParserConfigurationException, SAXException {
+        URL xmlURL = new URL("https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml");
+        InputStream xml = xmlURL.openStream();
+        String latestVersion = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xml).getElementsByTagName("latest").item(0).getTextContent();
+        xml.close();
+        return latestVersion;
+    }
+
+    /**
+     * Fetches the build id of the latest version of fabric-loader
+     * @return The build id of the latest version of fabric-loader
+     * @throws IOException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     */
+    private String getLatestFabricLoader() throws IOException, ParserConfigurationException, SAXException {
+        URL xmlURL = new URL("https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml");
+        InputStream xml = xmlURL.openStream();
+        String latestVersion = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xml).getElementsByTagName("latest").item(0).getTextContent();
+        xml.close();
+        return latestVersion.substring(latestVersion.indexOf("build"));
+    }
+
+    /**
+     * Fetches location of '.minecraft' folder
+     * @return Location of '.minecraft' folder on user's computer
+     */
+    private String getMinecraftFolder() {
         String operatingSystemName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
 
         if (operatingSystemName.contains("nux")) {
@@ -99,6 +220,10 @@ public class Installer extends JPanel {
 
     }
 
+    /**
+     *  Fetches location of 'instances' folder on user's computer
+     * @return (MultiMC) Location of 'instances' folder on user's computer
+     */
     private String getInstacesFolder() {
         String operatingSystemName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
 
@@ -112,68 +237,6 @@ public class Installer extends JPanel {
         }
 
         throw new RuntimeException("Cannot find instaces folder.");
-    }
-
-    private void installKami(String directory) throws IOException, SAXException, ParserConfigurationException {
-        //TODO: find and delete older kami versions
-        if (!hasLatestFabric(directory.substring(0, directory.length() - 5))) installFabric();
-
-        String latestVersion = getLatestKami();
-        System.out.println(latestVersion);
-
-        Path mods  = Paths.get(directory + latestVersion + ".jar");
-        InputStream in = new URL("https://github.com/zeroeightysix/KAMI/releases/download/" + latestVersion + "/kami-" + latestVersion + ".jar").openStream();
-
-        if (new File(String.valueOf(mods)).exists()) {
-            JOptionPane.showMessageDialog(null, "It looks like KAMI is already installed.");
-        } else {
-            Files.copy(in, mods, StandardCopyOption.REPLACE_EXISTING);
-            JOptionPane.showMessageDialog(null, "Installed KAMI to:\n"+directory);
-        }
-
-        //new File(System.getProperty("user.dir") + File.separator + "fabric-installer.jar").delete();
-    }
-
-    private boolean hasLatestFabric(String directory) {
-        //TODO: CHECK FOR LATEST VERSION OF FABRIC
-        Path fabric = Paths.get(directory+".fabric");
-        System.out.println(fabric.toString());
-        return Files.exists(fabric);
-
-    }
-
-    private void installFabric() throws IOException, SAXException, ParserConfigurationException {
-        URL xmlURL = new URL("https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml");
-        InputStream xml = xmlURL.openStream();
-        String latestVersion = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xml).getElementsByTagName("latest").item(0).getTextContent();
-        xml.close();
-
-        JOptionPane.showMessageDialog(null,"It looks like fabric is not installed, this will prompt you to install fabric version " + latestVersion);
-
-        Path fabricInstall = Paths.get(System.getProperty("user.dir") + File.separator + "fabric-installer.jar");
-        InputStream in = new URL("https://maven.fabricmc.net/net/fabricmc/fabric-installer/"+latestVersion+"/fabric-installer-"+latestVersion+".jar").openStream();
-        Files.copy(in, fabricInstall, StandardCopyOption.REPLACE_EXISTING);
-
-        Runtime.getRuntime().exec("java -jar fabric-installer.jar");
-    }
-
-    private String getLatestKami() throws IOException {
-        try (InputStream is = new URL("https://api.github.com/repos/zeroeightysix/KAMI/releases").openStream()) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            StringBuilder builder = new StringBuilder();
-
-            int currentCharacter;
-            while ((currentCharacter = reader.read()) != -1) {
-                builder.append((char) currentCharacter);
-            }
-            String jsonText = builder.toString();
-
-            // As is the case with the Kami Blue installer,
-            // because we can't import libraries that minecraft has and because
-            // the game isn't initialized we **have** to hard code this😔
-            return jsonText.split("tag_name", 2)[1].split("\"", 6)[2];
-
-        }
     }
 
 }
