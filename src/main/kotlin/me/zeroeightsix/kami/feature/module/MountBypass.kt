79f535bd7656d319e02975ebc6e25996bf2addfe
Dominika <sokolov.dominika@gmail.com>
2020-10-22 22:22:12 -0400

    [feature] Added MountBypass

---
+++ src/main/kotlin/me/zeroeightsix/kami/feature/module/MountBypass.kt
@@ -0,0 +1,20 @@
+package me.zeroeightsix.kami.feature.module
+
+import me.zero.alpine.listener.EventHandler
+import me.zero.alpine.listener.Listener
+import me.zeroeightsix.kami.event.PacketEvent
+import net.minecraft.entity.passive.AbstractDonkeyEntity
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket
+
+@Module.Info(
+    name = "MountBypass",
+    category = Module.Category.PLAYER,
+    description = "Forcefully allows mounting donkeys with chests"
+)
+object MountBypass : Module() {
+    @EventHandler
+    private val listener = Listener({ event: PacketEvent.Send ->
+        if (mc.player == null || mc.world == null || event.packet !is PlayerInteractEntityC2SPacket) return@Listener
+        if (event.packet.getEntity(mc.world) is AbstractDonkeyEntity) event.cancel()
+    })
+}
