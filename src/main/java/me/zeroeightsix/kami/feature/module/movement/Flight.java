f90b79eeaa86cc2275841503a0cb7c0ca2af1e8b
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-18 20:29:53 +0100

    Store and restore flight capabilties on enable/disable
    Fixes #502

--- src/main/java/me/zeroeightsix/kami/feature/module/movement/Flight.java
+++ src/main/java/me/zeroeightsix/kami/feature/module/movement/Flight.java
@@ -10,6 +10,7 @@ import me.zeroeightsix.kami.util.EntityUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.GameMode;
 
 @Module.Info(category = Module.Category.MOVEMENT, description = "Makes the player fly", name = "Flight")
 public class Flight extends Module {
@@ -18,6 +19,8 @@ public class Flight extends Module {
     private @Setting.Constrain.Range(min = 0, max = 10, step = 0.5) float speed = 1f;
     @Setting(name = "Mode")
     private FlightMode mode = FlightMode.VANILLA;
+
+    private transient FlightCapabilities capabilities;
 
     @EventHandler
     private Listener<TickEvent.InGame> updateListener = new Listener<>(event -> {
@@ -77,19 +80,43 @@ public class Flight extends Module {
     });
 
     @Override
+    public void onEnable() {
+        this.capabilities = mc.player != null ?
+                new FlightCapabilities(mc.player.abilities.flying, mc.player.abilities.getFlySpeed(), mc.player.abilities.allowFlying) :
+                FlightCapabilities.SURVIVAL;
+    }
+
+    @Override
     public void onDisable() {
         super.onDisable();
-
-        if (mode == FlightMode.VANILLA && mc.player != null) {
-            mc.player.abilities.flying = false;
-            mc.player.abilities.setFlySpeed(0.05f);
-            if (mc.player.abilities.creativeMode) return;
-            mc.player.abilities.allowFlying = false;
-        }
+        if (mc.player != null)
+            capabilities.apply();
     }
 
     public enum FlightMode {
         VANILLA, STATIC, PACKET
+    }
+
+    private static class FlightCapabilities {
+        final boolean flying;
+        final float flightSpeed;
+        final boolean allowFlying;
+
+        static FlightCapabilities SURVIVAL = new FlightCapabilities(false, 0.05f, false);
+
+        private FlightCapabilities(boolean flying, float flightSpeed, boolean allowFlying) {
+            this.flying = flying;
+            this.flightSpeed = flightSpeed;
+            this.allowFlying = allowFlying;
+        }
+
+        public void apply() {
+            assert mc.player != null;
+
+            mc.player.abilities.flying = this.flying;
+            mc.player.abilities.setFlySpeed(this.flightSpeed);
+            mc.player.abilities.allowFlying = this.allowFlying;
+        }
     }
 
 }
