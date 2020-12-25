9ce3fdb4aa167c612a6494c5c1fe0c7a73c41124
Humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-12-25 17:57:58 -0500

    fix build error

--- src/main/kotlin/me/zeroeightsix/kami/feature/module/Notebot.kt
+++ src/main/kotlin/me/zeroeightsix/kami/feature/module/Notebot.kt
@@ -4,13 +4,11 @@ import io.github.fablabsmc.fablabs.api.fiber.v1.annotation.Setting
 import me.zero.alpine.listener.EventHandler
 import me.zero.alpine.listener.Listener
 import me.zeroeightsix.kami.event.TickEvent
-import me.zeroeightsix.kami.mc
 import me.zeroeightsix.kami.mixin.client.IMinecraftClient
 import me.zeroeightsix.kami.util.InstrumentMap
 import me.zeroeightsix.kami.util.MidiParser
 import me.zeroeightsix.kami.util.Note
 import me.zeroeightsix.kami.util.Viewblock.getIrreplaceableNeighbour
-import me.zeroeightsix.kami.util.Wrapper
 import me.zeroeightsix.kami.util.plus
 import me.zeroeightsix.kami.util.text
 import net.minecraft.block.Blocks
@@ -22,8 +20,8 @@ import net.minecraft.client.world.ClientWorld
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket
 import net.minecraft.util.Hand
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
+import net.minecraft.world.World
 import java.io.File
 import java.util.ArrayList
 import java.util.TreeMap
@@ -44,33 +42,25 @@ object Notebot : Module() {
     var hatAlwaysAsFSharp = true
 
     @Setting(name = "Channel 0")
-    var ChannelZero = Instrument.HAT
+    var channelZero = Instrument.HAT
 
     @Setting(name = "Channel 1")
-    var ChannelOne = Instrument.HAT
+    var channelOne = Instrument.HAT
 
     @Setting(name = "Channel 2")
-    var ChannelTwo = Instrument.HAT
+    var channelTwo = Instrument.HAT
 
     @Setting(name = "Channel 3")
-    var ChannelThree = Instrument.HAT
+    var channelThree = Instrument.HAT
 
     @Setting(name = "Channel 4")
-    var ChannelFour = Instrument.HAT
-
-    private var channelsArray = arrayOf(ChannelZero, ChannelOne, ChannelTwo, ChannelThree, ChannelFour)
+    var channelFour = Instrument.HAT
 
     private var lastNote: Long = 0L
     private var elapsed: Long = 0L
 
     private var noteSequence: TreeMap<Long, ArrayList<Note>> = TreeMap()
     var instrumentMap = InstrumentMap()
-
-    enum class NotebotMode {
-        DISCOVER,
-        LOAD,
-        PLAY
-    }
 
     @EventHandler
     val listener = Listener<TickEvent.InGame>({
@@ -79,23 +69,27 @@ object Notebot : Module() {
 
         when (mode) {
             NotebotMode.DISCOVER -> {
-                for (x in -4..4) for (y in -4..4) for (z in -4..4) {
-                    val pos = player.pos?.plus(Vec3d(x.toDouble(), y.toDouble(), z.toDouble())) ?: break
-                    if (world.getBlockState(BlockPos(pos))?.block == Blocks.NOTE_BLOCK) {
-                        val noteBlock = world.getBlockState(BlockPos(pos))
-                        snackbarMessage(
-                            player, "" + noteBlock.get(INSTRUMENT) + "[" + noteBlock.get(NOTE) + "]"
-                        )
-                        map.add(noteBlock.get(INSTRUMENT), noteBlock.get(NOTE), BlockPos(pos))
+                for (x in -4..4) {
+                    for (y in -4..4) {
+                        for (z in -4..4) {
+                            val pos = player.pos?.plus(Vec3d(x.toDouble(), y.toDouble(), z.toDouble())) ?: break
+                            if (world.getBlockState(BlockPos(pos))?.block == Blocks.NOTE_BLOCK) {
+                                val noteBlock = world.getBlockState(BlockPos(pos))
+                                snackbarMessage(
+                                    player, "" + noteBlock.get(INSTRUMENT) + "[" + noteBlock.get(NOTE) + "]"
+                                )
+                                instrumentMap.add(noteBlock.get(INSTRUMENT), noteBlock.get(NOTE), BlockPos(pos))
+                            }
+                        }
                     }
                 }
             }
             NotebotMode.LOAD -> {
-                noteSequence = MidiParser.parse("songs" + File.separator + songName, 5)
+                noteSequence = MidiParser.parse("songs${File.separator}$songName", 5)
                 if (noteSequence.isEmpty())
                     snackbarMessage(player, "Song not found: " + "songs" + File.separator + songName)
                 else
-                    snackbarMessage(player, "Song found: songs${File.seperator}$songName")
+                    snackbarMessage(player, "Song found: songs${File.separator}$songName")
                 lastNote = System.currentTimeMillis()
                 elapsed = 0
             }
@@ -111,14 +105,15 @@ object Notebot : Module() {
         }
     })
 
-    private fun playNotes(notes: List<Note>, player: ClientPlayerEntity, world: World) {
+    private fun playNotes(notes: List<Note>, player: ClientPlayerEntity, world: ClientWorld) {
         val blockPosArr: ArrayList<BlockPos?> = ArrayList()
         notes.forEach { n ->
-            channelsArray = arrayOf(ChannelZero, ChannelOne, ChannelTwo, ChannelThree, ChannelFour)
+            val channelsArray: Array<Instrument> =
+                arrayOf(channelZero, channelOne, channelTwo, channelThree, channelFour)
             val number = (n.track % (channelsArray.size - 1))
             val enum = channelsArray[if (number < 0) 0 else number]
             snackbarMessage(player, enum.toString())
-            blockPosArr.add(map[enum][if (enum == Instrument.HAT && hatAlwaysAsFSharp) 0 else n.notebotNote])
+            blockPosArr.add(instrumentMap[enum][if (enum == Instrument.HAT && hatAlwaysAsFSharp) 0 else n.notebotNote])
         }
         playBlock(blockPosArr, player, world)
     }
@@ -146,3 +141,10 @@ object Notebot : Module() {
         }
     }
 }
+
+enum class NotebotMode {
+    DISCOVER,
+    LOAD,
+    PLAY
+}
+
