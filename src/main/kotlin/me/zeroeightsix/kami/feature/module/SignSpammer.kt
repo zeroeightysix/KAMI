aa334dd134bba5402f5ce75afbd4ba1847ee7989
LordMZTE <lord@mzte.de>
2020-12-30 16:31:25 +0100

    move javadoc above annotation

--- src/main/kotlin/me/zeroeightsix/kami/feature/module/SignSpammer.kt
+++ src/main/kotlin/me/zeroeightsix/kami/feature/module/SignSpammer.kt
@@ -2,14 +2,14 @@ package me.zeroeightsix.kami.feature.module
 
 import io.github.fablabsmc.fablabs.api.fiber.v1.annotation.Setting
 
+/**
+ * @see me.zeroeightsix.kami.mixin.client.MixinClientPlayNetworkHandler
+ */
 @Module.Info(
     name = "SignSpammer",
     category = Module.Category.MISC,
     description = "Automatically fills newly placed signs"
 )
-/**
- * @see me.zeroeightsix.kami.mixin.client.MixinClientPlayNetworkHandler
- */
 object SignSpammer : Module() {
     @Setting
     var line1 = ""
