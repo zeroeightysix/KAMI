5c2c3c26bf2c6a31cdf8222906e016455ff2438f
nopjmp <kthompson@hey.com>
2021-01-26 23:29:58 -0600

    fix: widget name must be unique and not null or empty

--- src/main/kotlin/me/zeroeightsix/kami/gui/widgets/VoidContextMenu.kt
+++ src/main/kotlin/me/zeroeightsix/kami/gui/widgets/VoidContextMenu.kt
@@ -1,6 +1,7 @@
 package me.zeroeightsix.kami.gui.widgets
 
 import imgui.ImGui
+import imgui.ImGui.text
 import imgui.flag.ImGuiCol
 import imgui.flag.ImGuiPopupFlags
 import imgui.flag.ImGuiWindowFlags
@@ -13,6 +14,7 @@ import me.zeroeightsix.kami.gui.ImguiDSL.popupModal
 import me.zeroeightsix.kami.gui.ImguiDSL.withStyleColour
 import me.zeroeightsix.kami.gui.View
 import me.zeroeightsix.kami.gui.windows.modules.Modules
+import java.util.*
 
 /**
  * The context menu that appears when the user right-clicks where there are no imgui windows
@@ -20,6 +22,7 @@ import me.zeroeightsix.kami.gui.windows.modules.Modules
 object VoidContextMenu {
 
     private var buffer = ImString()
+    private var errorText = ""
     private var widgetProducer: Pair<String, (String) -> Unit>? = null
 
     operator fun invoke() {
@@ -72,24 +75,45 @@ object VoidContextMenu {
             View()
         }
 
+        val takenNames = EnabledWidgets.widgets.map { it.name }.toSet()
         widgetProducer?.let { (title, factory) ->
             ImGui.openPopup(title) // Calling this in the menu for some reason doesn't work. So we spam it instead, because ImGui handles this user error!
             popupModal(title, extraFlags = ImGuiWindowFlags.AlwaysAutoResize) {
                 ImGui.inputText("Title", buffer)
 
+                if (errorText.isNotBlank()) {
+                    withStyleColour(ImGuiCol.Text, .7f, .3f, .3f, 1f) {
+                        text(errorText)
+                    }
+                }
+
                 withStyleColour(ImGuiCol.Text, .7f, .7f, .7f, 1f) {
                     button("Cancel", 100f, 0f) {
+                        errorText = ""
+                        buffer.set("")
                         widgetProducer = null
                         ImGui.closeCurrentPopup()
                     }
                 }
                 ImGui.sameLine()
                 button("Create", 100f, 0f) {
-                    val widgetName = buffer.get() ?: ""
-                    buffer.set("")
-                    factory(widgetName)
-                    widgetProducer = null
-                    ImGui.closeCurrentPopup()
+                    val widgetName = if (buffer.isEmpty) {
+                        // TODO: imgui doesn't allow null or no title names
+                        // bool ImGui::Begin(const char*, bool*, ImGuiWindowFlags): Assertion `name != __null && name[0] != '\0'' failed.
+                        UUID.randomUUID().toString()
+                    } else {
+                        buffer.get()
+                    }
+
+                    if (!takenNames.contains(widgetName)) {
+                        errorText = ""
+                        buffer.set("")
+                        factory(widgetName)
+                        widgetProducer = null
+                        ImGui.closeCurrentPopup()
+                    } else {
+                        errorText = "Title must be unique!"
+                    }
                 }
             }
         }
