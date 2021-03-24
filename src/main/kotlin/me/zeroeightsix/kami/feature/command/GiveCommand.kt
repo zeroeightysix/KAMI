56528ab975c7d7ecbbc6cc66cd8f6b93f915cabe
redstonekasi <42352565+redstonekasi@users.noreply.github.com>
2021-03-24 21:39:38 +0100

    Add give command (#538)
    * add give command
    
    * fix typo
    remove non-null safe call
    
    * local player variable
    
    Co-authored-by: kasimir <contact@redstonekasi.de>

---
+++ src/main/kotlin/me/zeroeightsix/kami/feature/command/GiveCommand.kt
@@ -0,0 +1,56 @@
+package me.zeroeightsix.kami.feature.command
+
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType
+import me.zeroeightsix.kami.mc
+import net.minecraft.command.CommandSource
+import net.minecraft.command.argument.ItemStackArgument
+import net.minecraft.command.argument.ItemStackArgumentType
+import net.minecraft.item.ItemStack
+import net.minecraft.text.LiteralText
+
+object GiveCommand : Command() {
+
+    private val FAILED_EXCEPTION =
+        DynamicCommandExceptionType { LiteralText(it.toString()) }
+
+    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
+        dispatcher register rootLiteral("give") {
+            argument("item", ItemStackArgumentType.itemStack()) {
+                does { ctx ->
+                    give("item" from ctx, 1)
+                    0
+                }
+
+                integer("count") {
+                    does { ctx ->
+                        give("item" from ctx, "count" from ctx)
+                        0
+                    }
+                }
+            }
+        }
+    }
+
+    private fun give(itemArgument: ItemStackArgument, count: Int) {
+        val player = mc.player ?: return
+
+        if (!player.isCreative) {
+            throw FAILED_EXCEPTION.create("You must be in creative mode to use this command")
+        }
+
+        val stack: ItemStack = itemArgument.createStack(count, false)
+
+        if (player.mainHandStack?.isEmpty == true) {
+            mc.interactionManager?.clickCreativeStack(stack, 36 + player.inventory?.selectedSlot!!)
+        } else {
+            val emptySlot: Int? = player.inventory?.emptySlot
+
+            if (emptySlot!! < 9) {
+                mc.interactionManager?.clickCreativeStack(stack, 36 + emptySlot)
+            } else {
+                mc.interactionManager?.clickCreativeStack(stack, 36 + player.inventory?.selectedSlot!!)
+            }
+        }
+    }
+}
